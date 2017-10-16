a19bef79ed50952452210cdb978a9012fa48eced
Yuji Sugiura <y.sugiura.0316@gmail.com>
2017-10-16 18:54:09 +0900

    Return promise

--- ms2hls-server/lib/util/ffmpeg.js
+++ ms2hls-server/lib/util/ffmpeg.js
@@ -5,15 +5,18 @@ const { rootPath } = require('./config');
 const webm2ts = function(inputPath, liveId, filename) {
   const outputPath = `${rootPath}/live/${liveId}/${filename.replace('.webm', '.ts')}`;
 
-  ffmpeg()
-    .input(inputPath)
-    // TODO: bitrate
-    .videoCodec('libx264')
-    .audioCodec('libfdk_aac')
-    .format('mpegts')
-    .output(outputPath)
-    .on('error', err => { throw err; })
-    .run();
+  return new Promise((resolve, reject) => {
+    ffmpeg()
+      .input(inputPath)
+      // TODO: bitrate
+      .videoCodec('libx264')
+      .audioCodec('libfdk_aac')
+      .format('mpegts')
+      .output(outputPath)
+      .on('error', err => reject(err))
+      .on('end', () => resolve())
+      .run();
+  });
 };
 
 module.exports = {
