23a554141210a5e1b78061f16805c5150370a848
Yuji Sugiura <y.sugiura.0316@gmail.com>
2017-10-16 10:05:25 +0900

    Generate playlist.m3u8

--- ms2hls-server/lib/api/finalize.js
+++ ms2hls-server/lib/api/finalize.js
@@ -1,14 +1,10 @@
-const { rootPath } = require('../util/config');
 const { writePlaylist } = require('../util/hls');
 
 module.exports = async function(request, reply) {
   const { liveId } = request.params;
 
-  const dirPath = `${rootPath}/live/${liveId}`;
-  dirPath;
-
   // TODO: make .m3u8
-  await writePlaylist(dirPath);
+  await writePlaylist(liveId);
 
   reply.code(200).send();
 };
--- ms2hls-server/lib/util/hls.js
+++ ms2hls-server/lib/util/hls.js
@@ -1,15 +1,16 @@
 const { writeFile } = require('./fs');
+const { rootPath, serverUrl } = require('./config');
 
-// TODO: bandwidth / resolution / another levels...
-const playlist = `
+const writePlaylist = function(liveId) {
+  // TODO: bandwidth / resolution / another levels...
+  const playlist = `
 #EXTM3U
 #EXT-X-VERSION:3
 #EXT-X-STREAM-INF:BANDWIDTH=1700000,CODECS="avc1.4d001f,mp4a.40.2",RESOLUTION=960x540
-http://localhost:9999/live/chunklist.m3u8
-`.trim();
+${serverUrl}/live/${liveId}/chunklist.m3u8
+  `.trim();
 
-const writePlaylist = function(dest) {
-  return writeFile(`${dest}/playlist.m3u8`, playlist);
+  return writeFile(`${rootPath}/live/${liveId}/playlist.m3u8`, playlist);
 };
 
 module.exports = {
